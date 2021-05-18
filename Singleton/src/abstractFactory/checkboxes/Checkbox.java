4e1091e009da57be4230a1e29e54cc0e390b43c2
Estefanía Martínez <zS19014006@estudiantes.uv.mx>
2021-05-17 19:46:00 -0500

    Abstract-Factory

---
+++ Singleton/src/abstractFactory/checkboxes/Checkbox.java
@@ -0,0 +1,9 @@
+
+package abstractFactory.checkboxes;
+
+/**
+ * Checkboxes is the second product family. It has the same variants as buttons.
+ */
+public interface Checkbox {
+    void paint();
+}
---
+++ Singleton/src/abstractFactory/factories/GUIFactory.java
@@ -0,0 +1,13 @@
+
+package abstractFactory.factories;
+
+import abstractFactory.buttons.Button;
+import abstractFactory.checkboxes.Checkbox;
+
+/**
+ * Abstract factory knows about all (abstract) product types.
+ */
+public interface GUIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+}
